8b2fdf9ce510739b7467a2c3607e0239e72affdc
Alex Lokk <highcatland@gmail.com>
2021-03-11 21:34:53 +0300

    update to use boilerpipe with image extractor

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from fnmatch import fnmatch
 from os.path import basename, exists, dirname, abspath, join
 from distutils.core import setup
 
-try:
-    from urllib import urlretrieve
-except:
-    from urllib.request import urlretrieve
-
 import sys
 if sys.version_info[0] < 3:
     print("This module can only be used with Python 3.")
@@ -16,24 +11,7 @@ if sys.version_info[0] < 3:
 
 __version__ = '1.3'
 boilerpipe_version = '1.2.0'
-DATAPATH = join(abspath(dirname((__file__))), 'src/boilerpipe/data')
 
-
-def download_jars(datapath, version=boilerpipe_version):
-    tgz_url = 'https://github.com/slaveofcode/boilerpipe3/raw/master/boilerpipe-{0}-bin.tar.gz'.format(version)
-    tgz_name = basename(tgz_url)
-
-    if not exists(tgz_name):
-        downloaded = urlretrieve(tgz_url, tgz_name)
-        tgz_name = downloaded[0]
-
-    tar = tarfile.open(tgz_name, mode='r:gz')
-    for tarinfo in tar.getmembers():
-        if not fnmatch(tarinfo.name, '*.jar'):
-            continue
-        tar.extract(tarinfo, datapath)
-
-download_jars(datapath=DATAPATH)
 
 setup(
     name='boilerpipe3',
@@ -42,8 +20,9 @@ setup(
     package_dir={'': 'src'},
     package_data={
         'boilerpipe': [
-            'data/boilerpipe-{version}/boilerpipe-{version}.jar'.format(version=boilerpipe_version),
-            'data/boilerpipe-{version}/lib/*.jar'.format(version=boilerpipe_version),
+            'data/boilerpipe-1.2.2/boilerpipe-core-1.2.2.jar',
+            'data/boilerpipe-1.2.2/README',
+            'data/boilerpipe-1.2.2/lib/*.jar',
         ],
     },
     install_requires=[
@@ -54,7 +33,7 @@ setup(
     author_email='zeandcode@gmail.com',
     maintainer = 'Aditya Kresna Permana',
     maintainer_email = 'zeandcode@gmail.com',
-    url = 'https://github.com/slaveofcode/boilerpipe3',
+    url = 'https://github.com/highcat/boilerpipe3',
     classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
--- src/boilerpipe/__init__.py
+++ src/boilerpipe/__init__.py
@@ -5,6 +5,10 @@ import jpype
 if jpype.isJVMStarted() != True:
     jars = []
     for top, dirs, files in os.walk(imp.find_module('boilerpipe')[1]+'/data'):
-        for nm in files:       
+        for nm in files:
             jars.append(os.path.join(top, nm))
-    jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=%s" % os.pathsep.join(jars))
+    jpype.startJVM(
+        jpype.getDefaultJVMPath(),
+        "-Djava.class.path=%s" % os.pathsep.join(jars),
+        convertStrings=True,
+    )
